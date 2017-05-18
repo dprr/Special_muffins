1a64f7a4c27513fa160529d7b4fa39474383d05d
Adi Brock <abrock@cs.huji.ac.il>
2017-05-18 15:14:48 +0300

    some server code

--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,40 @@
 import requests
 import json
+import random
+import signal
+import os
 
 server_name = 'https://special-muffins-12d5c.firebaseio.com'
 
-test = requests.get(server_name + '/test_noam.json')
+# test = requests.get(server_name + '/test_noam.json')
+# print(test.json())
 
-print(json.loads(test.content))
+
+def parse_range(alarm):
+    return 1, 10
 
 
+def randomize_time(alarm):
+    time_range = parse_range(alarm)
+    actual_time = random.randrange(time_range[0], time_range[1])
+    return actual_time
+
+
+def get_new_alarms():
+    requests.get(server_name + '/newNF/dnfId').json()
+
+
+
+def alarm_phone(signum, frame):
+    pass
+
+def set_alarm(alarm):
+
+
+
+def run_server():
+    while True:
+        new_alarms = get_new_alarms()
+        for alarm in new_alarms:
+            alarm_time = randomize_time(alarm)
+            break
