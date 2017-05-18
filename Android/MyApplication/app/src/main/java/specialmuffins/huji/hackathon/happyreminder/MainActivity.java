f83fec5fe1baf2d94af17ff015a51f79b071d9a2
dprr <dbfelber@gmail.com>
2017-05-18 10:11:05 +0300

    First commit

---
+++ Android/MyApplication/app/src/main/java/specialmuffins/huji/hackathon/happyreminder/MainActivity.java
@@ -0,0 +1,14 @@
+package specialmuffins.huji.hackathon.happyreminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //daniel
+    }
+}
